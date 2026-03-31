53e9aae089ec3c06c6ce58e4a2d32547d93ee544
Harika <kunchalaharika94@gmail.com>
2026-03-31 22:18:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/converter/",
+  base:"/currencyconverter/",
   plugins: [
     react({
       babel: {
