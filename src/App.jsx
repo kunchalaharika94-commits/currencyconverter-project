52fd10b7c39bc91b21296e4f28d5521de7b1a8f2
Harika <kunchalaharika94@gmail.com>
2026-03-31 21:25:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,40 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"; 
+import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import DashboardNavbar from "./Components/Dashboardnavbar";
 import Home from "./Components/Home";
 import Converter from "./Components/Converter";
 import Currencies from "./Components/Currencies";
 import PrivateRoute from "./Components/PrivateRoute";
+import Login from "./Components/Login"; // ✅ import Login
 
 function App() {
   const user = localStorage.getItem("user");
 
   return (
-    <HashRouterRouter>
-      {/* Navbar logic */}
+    <>
       {user ? <DashboardNavbar /> : <Navbar />}
 
       <Routes>
-        {/* Root route */}
+        {/* Root: redirect based on login status */}
         <Route path="/" element={
-          user ? <Navigate to="/home" replace /> : <Navigate to="/" replace />
+          user ? <Navigate to="/home" replace /> : <Navigate to="/login" replace />
         } />
-        
+
+        {/* Login route */}
+        <Route path="/login" element={<Login />} />
+
         {/* Protected Routes */}
         <Route path="/home" element={
-          <PrivateRoute>
-            <Home />
-          </PrivateRoute>
+          <PrivateRoute><Home /></PrivateRoute>
         } />
-
         <Route path="/converter" element={
-          <PrivateRoute>
-            <Converter />
-          </PrivateRoute>
+          <PrivateRoute><Converter /></PrivateRoute>
         } />
-
         <Route path="/currencies" element={
-          <PrivateRoute>
-            <Currencies />
-          </PrivateRoute>
+          <PrivateRoute><Currencies /></PrivateRoute>
         } />
       </Routes>
-    </HashRouter>
+    </>
   );
 }
 
